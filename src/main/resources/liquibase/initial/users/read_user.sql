765612c9830fa75c80caeb66d5b8101d422e4e47
Andrey Zakrzhevskiy <and.zak.one@gmail.com>
2021-12-31 03:45:40 +0300

    Fixing create db reader creation

--- src/main/resources/liquibase/initial/users/read_user.sql
+++ src/main/resources/liquibase/initial/users/read_user.sql
@@ -1,8 +1,7 @@
 -- changeset
 -- id:20211211-07 author:zakrzhevskiy-as
+CREATE USER reader WITH PASSWORD '$uper$ecret';
 GRANT USAGE ON SCHEMA vending_machine TO reader;
-ALTER USER reader WITH PASSWORD '$uper$ecret';
--- CREATE USER reader WITH PASSWORD '$uper$ecret';
 GRANT SELECT ON vending_machine.orders TO reader;
 GRANT SELECT ON vending_machine.users TO reader;
 GRANT SELECT ON vending_machine.orders_beverages TO reader;
